f8da811e488e5685f7e283036c43fd95b1b2f254
William Delo <55678535+wdelo@users.noreply.github.com>
2022-10-29 00:35:06 -0400

    Initial project commit

---
+++ Source/CatRunner/CatRunner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CatRunner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatRunner, "CatRunner" );
